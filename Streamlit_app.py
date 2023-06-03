90fb8630d894a776363339461c174b5a9eabd548
Soumyasrij <135439439+Soumyasrij@users.noreply.github.com>
2023-06-03 15:38:55 +0100

    Update Streamlit_app.py
    added header and additional item

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
                
-streamlit.text(' kale, spinach & rocket smoothie')
+streamlit.text('🥗 kale, spinach & rocket smoothie')
 
-streamlit.text('Hard Boiled free range egg')
+streamlit.text('🐔 Hard Boiled free range egg')
+
+streamlit.text('🥑🍞 Avacado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
